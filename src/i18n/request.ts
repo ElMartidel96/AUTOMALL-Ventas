70e3b0964c7b277dde55ffa7b9e32761f3ffdf25
ElMartidel96 <rafael1996k@gmail.com>
2026-02-21 22:33:37 -0600

    fix: set default locale to Spanish (es)
    Made by mbxarts.com The Moon in a Box property
    
    Co-Author: Godez22

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,7 +2,7 @@ import {getRequestConfig} from 'next-intl/server';
 import {cookies} from 'next/headers';
 
 export const locales = ['en', 'es'] as const;
-export const defaultLocale = 'en' as const;
+export const defaultLocale = 'es' as const;
 
 export type Locale = (typeof locales)[number];
 
